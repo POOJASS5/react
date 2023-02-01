9d5cdc4ffc9d7c4227385ca2772a3abe73c20855
poojaANUP <poojaanup522@gmail.com>
2023-02-01 12:59:31 +0530

    start

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem(){
+    return(
+    <div>
+        <div>Expense items</div>
+        <div>
+            
+            <div>food rs 10</div>
+            <div>petrol rs 100</div>
+            <div>movies rs 200</div>
+        </div>
+    </div>
+    )
+}
+
+        
+   
+
+export default ExpenseItem;
